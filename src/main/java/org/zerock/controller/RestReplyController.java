b8029238c85e5fb5a3c44104cc211210d7d382a5
Beginner <dkstmdwn17@naver.com>
2022-03-22 00:18:34 +0900

    REST 방식으로 CRUD구현
    REST 방식으로 CRUD구현.
    이전에 했던 Security Project에서 가지고  옴.

--- src/main/java/org/zerock/controller/RestReplyController.java
+++ src/main/java/org/zerock/controller/RestReplyController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
@@ -48,5 +51,42 @@ public class RestReplyController {
 	public ResponseEntity<List<ReplyVO>>getList(){
 		log.info("getList............");
 		return new ResponseEntity<>(service.getList(), HttpStatus.OK);
+	}
+	
+	// Read 개별 댓글 확인
+	@GetMapping(value="/{bid}",
+			produces = {MediaType.APPLICATION_XML_VALUE,
+						MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<ReplyVO> get(@PathVariable("bid") int bid){
+		log.info("get : " + bid);
+		
+		return new ResponseEntity<>(service.get(bid), HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH},
+			value = "/{bid}",
+			consumes = "application/json",
+			produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> modify(
+					@RequestBody ReplyVO vo,
+					@PathVariable("bid") int bid){
+		vo.setBid(bid);
+		
+		log.info("bid : " + bid);
+		
+		log.info("modify : " + vo);
+		
+		return service.modify(vo) == 1
+					? new ResponseEntity<>("success", HttpStatus.OK)
+					: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@DeleteMapping(value="/{bid}", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("bid") int bid){
+		log.info("remove: " + bid);
+		
+		return service.delete(bid) == 1
+		  ? new ResponseEntity<>("success", HttpStatus.OK)
+		  : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
